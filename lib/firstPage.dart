4d4973919d19f345d8577996d065416c1fa93541
infiniteoverflow <aswingopinathan1871@gmail.com>
2020-08-26 23:01:38 +0530

    Added the screens

---
+++ lib/firstPage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  FirstPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35.0,color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/secondPage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  SecondPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35.0,color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
